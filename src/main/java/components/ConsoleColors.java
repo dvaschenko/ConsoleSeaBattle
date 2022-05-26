7232b1d8220b0d2385409fde046c46025c45bbf6
Dmitrii Vashchenko <Dmitrii_Vashchenko@epam.com>
2022-05-26 23:12:53 +0300

    Init commit

---
+++ src/main/java/components/ConsoleColors.java
@@ -0,0 +1,12 @@
+package components;
+
+public final class ConsoleColors {
+
+        public static final String RESET = "\033[0m";
+        public static final String RED = "\033[0;31m";
+        public static final String GREEN = "\033[0;32m";
+        public static final String YELLOW = "\033[0;33m";
+        public static final String BLUE = "\033[0;34m";
+
+    }
+
---
+++ src/main/java/enums/State.java
@@ -0,0 +1,23 @@
+package enums;
+
+public enum State {
+    DECK('#'),
+    MISS('*'),
+    HIT('X');
+
+    private final char state;
+
+    State(char state) {
+        this.state = state;
+    }
+
+    public char getState() {
+        return state;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(state);
+    }
+}
+
